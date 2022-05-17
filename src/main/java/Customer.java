3affa5f1f311a6ac1972f0f57c213b3be363aea3
blitzdose <blitzdose@gmail.com>
2022-05-17 09:38:09 +0200

    STEP: Moved amountFor to Rental

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -28,7 +28,7 @@ class Customer {
             double rentalAmount = 0;
             Rental aRental = (Rental) rentalEnumeration.nextElement();
             //determine amounts for each line
-            rentalAmount = amountFor(aRental);
+            rentalAmount = aRental.getCharge();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -42,26 +42,6 @@ class Customer {
         returnString += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         returnString += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return returnString;
-    }
-
-    private double amountFor(Rental aRental) {
-        double result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
     }
 
 }
